8f36c93f7a188be78539db308fa163e5471ae434
irdacin <nichodri22@gmail.com>
2024-08-19 22:06:06 +0700

    Solve Pusing Makan-Makan

---
+++ Paradigma Penyelesaian Masalah/C. Dynamic Programming/H. Pusing Makan-Makan.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define rcout(x) return void(cout << x)
+
+template <typename T, typename U> istream& operator>>(istream& is, pair<T, U>& _p) { is >> _p.first >> _p.second; return is; }
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U> _p) { os << _p.first << ' ' << _p.second; return os; }
+template <typename T> istream& operator>>(istream& is, vector<T>& _v) { for(auto &_i : _v) is >> _i; return is; }
+template <typename T> ostream& operator<<(ostream& os, const vector<T> _v) { for(auto &_i : _v) os << _i << " \n"[&_i == &_v.back()]; return os; }
+template <typename T, typename U> ostream& operator<<(ostream& os, const vector<pair<T, U>> _v) { for(auto _i : _v) os << _i << endl; return os; }
+template <typename T> struct vec : vector<T> { using vector<T>::vector; };
+template <typename T> struct vvec : vec<vec<T>> {
+    vvec(initializer_list<initializer_list<T>> _list) { for(const auto& _row : _list) this->emplace_back(_row); }
+    vvec(int _n = 0, int _m = 0, const T& _init_value = T()) : vec<vec<T>>(_n, vec<T>(_m, _init_value)) {}
+    friend ostream& operator<<(ostream &os, const vvec _v) { for(auto _i : _v) os << _i; return os; }
+};
+template <typename T> struct vvvec : vec<vvec<T>> {
+    vvvec(initializer_list<initializer_list<initializer_list<T>>> _list) { for(const auto& _rows : _list) for(const auto& _row : _rows) this->emplace_back(_row); }
+    vvvec(int _n = 0, int _m = 0, int _k = 0, const T& _init_value = T()) : vec<vvec<T>>(_n, vvec<T>(_m, _k, _init_value)) {}
+    friend ostream& operator<<(ostream &os, const vvvec _v) { for(auto _i : _v) os << _i << endl; return os; }
+};
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define len(x) (int) x.size()
+using pii = pair<int, int>;
+using iii = tuple<int, int, int>;
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+
+void solve(){
+    int n; cin >> n;
+    vec<pii> pesta(n);
+    for(auto &[s, e] : pesta) cin >> s >> e;
+    sort(all(pesta)); 
+    
+    vec<int> dp(n, -1);
+    auto func = [&](auto func, int i) -> int {
+        if(i == n) return 0;
+
+        int &res = dp[i];
+        if(res != -1) return res;
+
+        res = 1;
+
+        auto [si, ei] = pesta[i];
+        // compute next interval first
+        (res += func(func, i + 1)) %= MOD;
+
+        // using binser to find the first index where the start time
+        // is greater than to the end time of the current interval
+
+        int next = n;
+        for(int left = i + 1, right = n - 1; left <= right;){
+            int mid = (left + right) / 2;
+            auto [sj, ej] = pesta[mid];
+
+            if(sj > ei) next = mid, right = mid - 1;
+            else left = mid + 1;
+        }
+
+        (res += func(func, next)) %= MOD;
+        return res;
+    };
+
+    cout << func(func, 0);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // int t; cin >> t; while(t--)
+    solve(), cout << endl;
+    // solve();
+}
