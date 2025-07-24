68bb240eb61b0401bc908e8918c79c53c1539f74
irdacin <nichodri22@gmail.com>
2025-07-24 19:34:12 +0700

    Add solution to problem B Perkenalan Knapsack

--- tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/B. Perkenalan Knapsack.cpp
+++ tlx/Pemrograman Kompetitif Dasar/07. Dynamic Programming/B. Perkenalan Knapsack.cpp
@@ -4,25 +4,27 @@ using namespace std;
 #define int long long
 #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define endl '\n'
-#define debug(x) cout << #x << " => " << x << endl
-
-template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& _p) {
-  os << _p.first << ' ' << _p.second; return os;
+#define debug(x) cout << #x << " => " << (x) << endl
+ 
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& _p) { 
+  os << "(" << _p.first << ", " << _p.second << ")"; return os; 
 }
 template <typename T> istream& operator>>(istream& is, vector<T>& _v) {
-  for(auto &_i : _v) { is >> _i; } return is;
+  for (auto& _x : _v) { is >> _x; } return is;
 }
-template <typename T> ostream& operator<<(ostream& os, const vector<T>& _v) {
-  for(auto &_i : _v) { os << _i; if(&_i != &_v.back()) os << ' '; } return os;
+template <typename Ch, typename Tr, typename Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, const Container& _c) {
+  int _i = 0; os << "{ "; for (const auto& _x : _c) { os << (_i++ ? ", " : "") << _x; }
+  return os << " }";
 }
 inline void print() { cout << endl; }
 template <typename T = const char*, typename... U> 
-inline void prin(T &&A = "", U &&...B) { cout << A; if (sizeof...(U)) prin(B...); }
+inline void prin(T&& A = "", U&&... B) { cout << A; if (sizeof...(U)) prin(B...); }
 template <typename T, typename... U>
-inline void print(T &&A, U &&...B) { prin(A); if (sizeof...(U)) prin(' '); print(B...); }
-template <typename T> inline void printsp(T &&A, const char* sp = " ") { prin(A, sp); }
+inline void print(T&& A, U&&... B) { prin(A); if (sizeof...(U)) prin(' '); print(B...); }
+template <typename T> inline void prinsp(T&& A, char sp = ' ') { prin(A, sp); }
 
-template <typename T> struct vec : vector<T> { using vector<T>::vector; };
+template <typename T> using vec = vector<T>;
 template <typename T> struct vvec : vec<vec<T>> {
   using vec<vec<T>>::vec;
   vvec(int N = 0, int M = 0, const T& _init_value = T())
@@ -39,71 +41,48 @@ using iii = tuple<int, int, int>;
 #define fi first
 #define se second
 #define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define len(x) (int) x.size()
+#define all(x) (x).begin(), (x).end()
+#define len(x) (int)(x).size()
 
-const int MOD = 1e9 + 7;
+const int MOD = 1e9 + 7; // 998244353;
 const int INF = 2e9;
-const int MAXN = 2e5;
 
-void solve(){
+void solve() {
   int n, k; cin >> n >> k;
   vec<pii> stone(k);
   for(auto &[w, h] : stone) cin >> w >> h;
 
   vvec<int> dp(k + 1, n + 1);
-  for(int i = k - 1; i >= 0; i--){
+
+  for(int i = k - 1; i >= 0; i--) {
     auto [w, h] = stone[i];
-    for(int capacity = 0; capacity <= n; capacity++){
+    
+    for(int capacity = 0; capacity <= n; capacity++) {
       dp[i][capacity] = dp[i + 1][capacity];
-      if(capacity >= w && dp[i][capacity] <= dp[i + 1][capacity - w] + h){
-        dp[i][capacity] = dp[i + 1][capacity - w] + h;
+
+      if(capacity >= w) {
+        dp[i][capacity] = max(dp[i][capacity], dp[i + 1][capacity - w] + h);
       }
     }
   }
 
-  // cari total beratnya sekecil mungkin
   int res = dp[0][n];
   for(; res == dp[0][n - 1]; n--);
-  
-  // utamakan batu dengan nomor lebih kecil
-  for(int i = 0; i < k; i++){
+
+  for(int i = 0; i < k; i++) {
     auto [w, h] = stone[i];
-    if(n >= w && dp[i][n] == dp[i + 1][n - w] + h){
+    if(n >= w && dp[i][n] == dp[i + 1][n - w] + h) {
       print(i + 1);
       n -= w;
     }
   }
 }
 
-signed main(){
+signed main() {
   fastio;
-  // #undef endl
+  // cout << fixed << setprecision(6);
 
   // precompute();
-  // cout << setprecision(6) << fixed;
   // int t; cin >> t; while(t--)
   solve();
 }
-
-/*
-
-11 4
-4 5
-5 5
-5 5
-3 5
-
-1
-4
-
-
-5 4
-4 4
-3 3
-2 2
-1 1
-
-1
-4
-*/
