da8e43c2656bf7fe022ab59c39c1ea33135d23c1
irdacin <nichodri22@gmail.com>
2025-08-05 20:52:08 +0700

    Add solution to problem D Perkenalan Breadth-First Search

---
+++ tlx/Pemrograman Kompetitif Dasar/09. Perkenalan Graf/D. Perkenalan Breadth-First Search.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define debug(x) cout << #x << " => " << (x) << endl
+ 
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& _p) { 
+  os << "(" << _p.first << ", " << _p.second << ")"; return os; 
+}
+template <typename T> istream& operator>>(istream& is, vector<T>& _v) {
+  for (auto& _x : _v) { is >> _x; } return is;
+}
+template <typename Ch, typename Tr, typename Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, const Container& _c) {
+  size_t _i = 0; os << "{ "; for (const auto& _x : _c) { os << (_i++ ? ", " : "") << _x; }
+  return os << " }";
+}
+inline void print() { cout << endl; }
+template <typename T = const char*, typename... U> 
+inline void prin(T&& A = "", U&&... B) { cout << A; if (sizeof...(U)) prin(B...); }
+template <typename T, typename... U>
+inline void print(T&& A, U&&... B) { prin(A); if (sizeof...(U)) prin(' '); print(B...); }
+template <typename T> inline void prinsp(T&& A, char sp = ' ') { prin(A, sp); }
+
+template <typename T> using vec = vector<T>;
+template <typename T> struct vvec : vec<vec<T>> {
+  using vec<vec<T>>::vec;
+  vvec(size_t N = 0, size_t M = 0, const T& _init_value = T())
+    : vec<vec<T>>(N, vec<T>(M, _init_value)) {}
+};
+template <typename T> struct vvvec : vec<vvec<T>> {
+  using vec<vvec<T>>::vec;
+  vvvec(size_t N = 0, size_t M = 0, size_t K = 0, const T& _init_value = T())
+    : vec<vvec<T>>(N, vvec<T>(M, K, _init_value)) {}
+};
+
+using pii = pair<int, int>;
+using iii = tuple<int, int, int>;
+#define fi first
+#define se second
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define len(x) (int)(x).size()
+
+const int MOD = 1e9 + 7; // 998244353;
+const int INF = 2e9;
+
+void solve() {
+  int n; cin >> n;
+  vec<int> a(n); cin >> a;
+  int k; cin >> k;
+
+  map<vec<int>, int> dist;
+  queue<vec<int>> q;
+  q.emplace(a);
+  dist[a] = 0;
+
+  while(!q.empty()) {
+    auto cur = q.front(); q.pop();
+    if(is_sorted(all(cur))) return print(dist[cur]);
+
+    for(int i = 0; i + k <= n; i++) {
+      vec<int> nxt = cur;
+      for(int le = i, ri = i + k - 1; le < ri; le++, ri--) swap(nxt[le], nxt[ri]);
+      if(dist.find(nxt) == dist.end()) {
+        dist[nxt] = dist[cur] + 1;
+        q.emplace(nxt);
+      }
+    }
+  }
+
+  print(-1);
+}
+
+signed main() {
+  fastio;
+  // cout << fixed << setprecision(6);
+
+  // precompute();
+  // int t; cin >> t; while(t--)
+  solve();
+}
