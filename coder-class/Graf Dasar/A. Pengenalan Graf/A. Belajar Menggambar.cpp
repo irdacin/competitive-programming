df164891d28ca94a2fe1182a444ceb59ed59a9c7
irdacin <nichodri22@gmail.com>
2025-08-09 18:50:12 +0700

    Add solution to problem A Belajar Menggambar

--- coder-class/Graf Dasar/A. Pengenalan Graf/A. Belajar Menggambar.cpp
+++ coder-class/Graf Dasar/A. Pengenalan Graf/A. Belajar Menggambar.cpp
@@ -1,47 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define endl '\n'
-#define rcout(x) return void(cout << x)
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-template <typename T, typename U> istream& operator>>(istream& is, pair<T, U>& _p) { is >> _p.first >> _p.second; return is; }
-template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U> _p) { os << _p.first << ' ' << _p.second; return os; }
-template <typename T> istream& operator>>(istream& is, vector<T>& _v) { for(auto &_i : _v) is >> _i; return is; }
-template <typename T> ostream& operator<<(ostream& os, const vector<T> _v) { for(auto &_i : _v) os << _i << " \n"[&_i == &_v.back()]; return os; }
-template <typename T, typename U> ostream& operator<<(ostream& os, const vector<pair<T, U>> _v) { for(auto _i : _v) os << _i << endl; return os; }
-template <typename T> struct vec : vector<T> { using vector<T>::vector; };
-template <typename T> struct vvec : vec<vec<T>> {
-    vvec(initializer_list<initializer_list<T>> _list) { for(const auto& _row : _list) this->emplace_back(_row); }
-    vvec(int _n = 0, int _m = 0, const T& _init_value = T()) : vec<vec<T>>(_n, vec<T>(_m, _init_value)) {}
-    friend ostream& operator<<(ostream &os, const vvec _v) { for(auto _i : _v) os << _i; return os; }
-};
-template <typename T> struct vvvec : vec<vvec<T>> {
-    vvvec(initializer_list<initializer_list<initializer_list<T>>> _list) { for(const auto& _rows : _list) for(const auto& _row : _rows) this->emplace_back(_row); }
-    vvvec(int _n = 0, int _m = 0, int _k = 0, const T& _init_value = T()) : vec<vvec<T>>(_n, vvec<T>(_m, _k, _init_value)) {}
-    friend ostream& operator<<(ostream &os, const vvvec _v) { for(auto _i : _v) os << _i << endl; return os; }
-};
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define len(x) (int) x.size()
-using pii = pair<int, int>;
-using iii = tuple<int, int, int>;
-#define fi first
-#define se second
+  int n, m; cin >> n >> m;
 
-const int MOD = 1e9 + 7;
-const int INF = 2e9;
+  vector<vector<int>> adj(n);
+  for(int u, v; m--;) {
+    cin >> u >> v;
+    u--, v--;
+    adj[u].emplace_back(v);
+    adj[v].emplace_back(u);
+  }
 
-void solve(){
-    
-}
+  int start = -1;
+  for(int i = 0; i < n && start == -1; i++) {
+    if(!adj[i].empty()) start = i;
+  }
 
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+  if(start == -1) {
+    cout << "Tipe A\n";
+    return 0;
+  }
 
-    // int t; cin >> t; while(t--)
-    solve(), cout << endl;
-    // solve();
+  queue<int> q;
+  vector<bool> vis(n, false);
+  vis[start] = true;
+  q.emplace(start);
+
+  while(!q.empty()) {
+    int u = q.front(); q.pop();
+
+    for(auto v : adj[u]) {
+      if(!vis[v]) {
+        vis[v] = true;
+        q.emplace(v);
+      }
+    }
+  }
+
+  int odd = 0;
+  for(int i = 0; i < n; i++) {
+    if(!vis[i]) {
+      cout << "Tipe C\n";
+      return 0;
+    }
+
+    odd += adj[i].size() & 1;
+  }
+
+  if(odd == 0) cout << "Tipe A\n";
+  else if(odd == 2) cout << "Tipe B\n";
+  else cout << "Tipe C\n";
+
+  return 0;
 }
