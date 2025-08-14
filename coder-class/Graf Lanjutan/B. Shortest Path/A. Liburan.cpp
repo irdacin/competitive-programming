33bc71b3aa8cec47f729ead5d265f7dabe32b3df
irdacin <nichodri22@gmail.com>
2025-08-14 19:15:21 +0700

    Add solution to problem A Liburan

---
+++ coder-class/Graf Lanjutan/B. Shortest Path/A. Liburan.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define debug(x) cout << #x << " => " << (x) << endl
+ 
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& _p) { 
+  os << "(" << _p.first << ", " << _p.second << ")"; return os; 
+}
+template <typename T> istream& operator>>(istream& is, vector<T>& _v) {
+  for (auto& _x : _v) { is >> _x; } return is;
+}
+template <typename Ch, typename Tr, typename Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, const Container& _c) {
+  size_t _i = 0; os << "{ "; for (const auto& _x : _c) { os << (_i++ ? ", " : "") << _x; }
+  return os << " }";
+}
+inline void print() { cout << endl; }
+template <typename T = const char*, typename... U> 
+inline void prin(T&& A = "", U&&... B) { cout << A; if (sizeof...(U)) prin(B...); }
+template <typename T, typename... U>
+inline void print(T&& A, U&&... B) { prin(A); if (sizeof...(U)) prin(' '); print(B...); }
+template <typename T> inline void prinsp(T&& A, char sp = ' ') { prin(A, sp); }
+
+template <typename T> using vec = vector<T>;
+template <typename T> struct vvec : vec<vec<T>> {
+  using vec<vec<T>>::vec;
+  vvec(size_t N = 0, size_t M = 0, const T& _init_value = T())
+    : vec<vec<T>>(N, vec<T>(M, _init_value)) {}
+};
+template <typename T> struct vvvec : vec<vvec<T>> {
+  using vec<vvec<T>>::vec;
+  vvvec(size_t N = 0, size_t M = 0, size_t K = 0, const T& _init_value = T())
+    : vec<vvec<T>>(N, vvec<T>(M, K, _init_value)) {}
+};
+
+using pii = pair<int, int>;
+using iii = tuple<int, int, int>;
+#define fi first
+#define se second
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define len(x) (int)(x).size()
+
+const int MOD = 1e9 + 7; // 998244353;
+const int INF = 2e18;
+
+void solve() {
+  int n, m; cin >> n >> m;
+  vvec<pii> adj(n);
+  for(int u, v, w; m--;) cin >> u >> v >> w, u--, v--, adj[u].eb(v, w), adj[v].eb(u, w);
+
+  priority_queue<pii, vec<pii>, greater<>> pq;
+  vec<int> dist(n, INF);
+  dist[0] = 0;
+  pq.emplace(0, 0);
+
+  while(!pq.empty()) {
+    auto [dist_u, u] = pq.top(); pq.pop();
+
+    if(u + 1 == n) return print(dist[u]);
+    if(dist_u > dist[u]) continue;
+
+    for(auto [v, w] : adj[u]) {
+      if(dist[u] + w < dist[v]) {
+        dist[v] = dist[u] + w;
+        pq.emplace(dist[v], v);
+      }
+    }
+  }
+}
+
+signed main() {
+  fastio;
+  // cout << fixed << setprecision(6);
+
+  // precompute();
+  // int t; cin >> t; while(t--)
+  solve();
+}
