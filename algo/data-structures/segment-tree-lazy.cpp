be93a9e4821ccf056d5eb2820619a25a84a88edf
irdacin <nichodri22@gmail.com>
2025-09-17 22:40:04 +0700

    refactor: split out node and tag as a separate struct

--- algo/data-structures/segment-tree-lazy.cpp
+++ algo/data-structures/segment-tree-lazy.cpp
@@ -1,38 +1,5 @@
+template <class node, class tag>
 struct SegmentTree {
-  struct node {
-    int val;
-    node(int v = 0) : val(v) {}
-
-    friend node merge(const node& left, const node& right) {
-      node res;
-      res.val = left.val + right.val;
-      return res;
-    }
-
-    friend ostream& operator<<(ostream &os, const node& n) {
-      return os << "(" << n.val << ")";
-    }
-  };
-
-  struct tag {
-    int val;
-
-    tag() : val(0) {}
-    tag(int v) : val(v) {}
-
-    bool empty() { 
-      return val == 0;
-    }
-    
-    void apply(node& t, int k) {
-      t.val += k * val;
-    }
-
-    void combine(const tag& t) {
-      val += t.val;
-    }
-  };
-
   int n, h;
   vec<node> d;
   vec<tag> lz;
@@ -43,8 +10,8 @@ struct SegmentTree {
     lz.resize(n);
   }
 
-  SegmentTree(const vec<int>& v) : SegmentTree(len(v)) {
-    for(int i = 0; i < n; i++) d[i + n] = node(v[i]);
+  SegmentTree(const vec<node>& v) : SegmentTree(len(v)) {
+    for(int i = 0; i < n; i++) d[i + n] = v[i];
     build();
   }
 
@@ -53,7 +20,6 @@ struct SegmentTree {
   }
 
   void apply(int id, tag& t, int k = 1) {
-    if(t.empty()) return;
     t.apply(d[id], k);
     if(id < n) lz[id].combine(t);
   }
@@ -68,14 +34,14 @@ struct SegmentTree {
     d[id] = merge(d[id << 1], d[id << 1 | 1]);
   }
 
-  void _update(int id, tag value) {
+  void update(int id, tag value) { // [id]
     id += n;
     for(int i = h; i; i--) push(id >> i, 1 << i);
     apply(id, value);
     for(id >>= 1; id; id >>= 1) pull(id);
   }
 
-  void _update(int l, int r, tag value) { // [l, r)
+  void update(int l, int r, tag value) { // [l, r)
     l += n, r += n;
 
     for(int i = h; i; i--) {
@@ -94,13 +60,13 @@ struct SegmentTree {
     }
   }
 
-  node _query(int id) {
+  node query(int id) { // [id]
     id += n;
     for(int i = h; i; i--) push(id >> i, 1 << i);
     return d[id];
   }
 
-  node _query(int l, int r) { // [l, r)
+  node query(int l, int r) { // [l, r)
     l += n, r += n;
 
     for(int i = h; i; i--) {
@@ -116,7 +82,30 @@ struct SegmentTree {
 
     return merge(resL, resR);
   }
+};
 
-  void update(int l, int r, int val) { _update(l, r, tag(val)); }
-  int query(int l, int r) { return _query(l, r).val; }
+struct node {
+  int val;
+  node(int v = 0) : val(v) {}
+
+  friend node merge(const node& left, const node& right) {
+    node res;
+    res.val = left.val + right.val;
+    return res;
+  }
+};
+
+struct tag {
+  int val;
+
+  tag() : val(0) {}
+  tag(int v) : val(v) {}
+  
+  void apply(node& t, int k) {
+    t.val += k * val;
+  }
+
+  void combine(const tag& t) {
+    val += t.val;
+  }
 };
