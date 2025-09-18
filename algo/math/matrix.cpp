9e8309c7c622079eaeac8ba5e4aa08e828770927
irdacin <nichodri22@gmail.com>
2025-09-18 22:06:54 +0700

    refactor: remove assert checking to speed up

--- algo/math/matrix.cpp
+++ algo/math/matrix.cpp
@@ -4,27 +4,25 @@ struct matrix : vvec<mint> {
   matrix& operator*=(const matrix& other) {
     int n = len((*this));
     int m = len((*this)[0]);
-    assert(m == len(other));
 
     matrix res(n, len(other[0]));
     for(int i = 0; i < n; i++)
       for(int j = 0; j < len(other[0]); j++)
         for(int k = 0; k < m; k++)
           res[i][j] += (*this)[i][k] * other[k][j];
-    return *this = res;
+    return *this = move(res);
   }
 
-  matrix operator*(const matrix& other) const { 
-    return matrix(*this) *= other; 
+  matrix operator*(const matrix& other) const {
+    return matrix(*this) *= other;
   }
 
   friend matrix pow(matrix mat, int k) {
     int n = len(mat);
-    assert(n == len(mat[0]));
 
     matrix res(n, n);
     for(int i = 0; i < n; i++) res[i][i] = 1;
-    for(; k > 0; k /= 2, mat *= mat) if(k & 1) res *= mat;
+    for(; k > 0; k >>= 1, mat *= mat) if(k & 1) res *= mat;
     return res;
   }
 };
