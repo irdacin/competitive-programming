4af793dd313cfe1f3f35eb3c0259a646010a83e8
irdacin <nichodri22@gmail.com>
2025-09-18 22:01:13 +0700

    refactor: simplify into mint and fix return datatype

--- algo/math/mod-int-dynamic.cpp
+++ algo/math/mod-int-dynamic.cpp
@@ -1,10 +1,10 @@
-struct ModInt {
+struct mint {
   using u128 = __uint128_t;
   static inline uint64_t mod, mu;
   uint64_t val;
 
-  ModInt() : val(0) {}
-  ModInt(int v) { if (v < 0) (v %= (int)mod) += mod; val = reduce(v); }
+  mint() : val(0) {}
+  mint(int v) { if (v < 0) (v %= (int)mod) += mod; val = reduce(v); }
   explicit operator int() const { return val; }
   static void set_mod(int m) { mod = m, mu = -1ULL / m; }
   static inline uint64_t reduce(u128 x) {
@@ -13,33 +13,31 @@ struct ModInt {
     return r >= mod ? r - mod : r;
   }
 
-  ModInt& operator+=(const ModInt& other) { return (val += other.val) >= mod ? val -= mod : val, *this; }
-  ModInt& operator-=(const ModInt& other) { return (val -= other.val) >= mod ? val += mod : val, *this; }
-  ModInt& operator*=(const ModInt& other) { return val = reduce(u128(val) * other.val), *this; }
-  ModInt& operator/=(const ModInt& other) { return *this *= inv(other.val); }
-  ModInt& operator++() { return *this += 1; }
-  ModInt& operator--() { return *this -= 1; }
-  ModInt operator++(int32_t) { ModInt mi = *this; ++*this; return mi; }
-  ModInt operator--(int32_t) { ModInt mi = *this; --*this; return mi; }
-  ModInt operator+() const { return *this; }
-  ModInt operator-() const { return ModInt() -= *this; }
-  friend ModInt operator+(const ModInt& lhs, const ModInt& rhs) { return ModInt(lhs) += rhs; }
-  friend ModInt operator-(const ModInt& lhs, const ModInt& rhs) { return ModInt(lhs) -= rhs; }
-  friend ModInt operator*(const ModInt& lhs, const ModInt& rhs) { return ModInt(lhs) *= rhs; }
-  friend ModInt operator/(const ModInt& lhs, const ModInt& rhs) { return ModInt(lhs) /= rhs; }
+  mint& operator+=(const mint& other) { return (val += other.val) >= mod ? val -= mod : val, *this; }
+  mint& operator-=(const mint& other) { return (val -= other.val) >= mod ? val += mod : val, *this; }
+  mint& operator*=(const mint& other) { return val = reduce(u128(val) * other.val), *this; }
+  mint& operator/=(const mint& other) { return *this *= inv(other); }
+  mint& operator++() { return *this += 1; }
+  mint& operator--() { return *this -= 1; }
+  mint operator++(int32_t) { mint mi = *this; ++*this; return mi; }
+  mint operator--(int32_t) { mint mi = *this; --*this; return mi; }
+  mint operator+() const { return *this; }
+  mint operator-() const { return mint() -= *this; }
+  friend mint operator+(const mint& lhs, const mint& rhs) { return mint(lhs) += rhs; }
+  friend mint operator-(const mint& lhs, const mint& rhs) { return mint(lhs) -= rhs; }
+  friend mint operator*(const mint& lhs, const mint& rhs) { return mint(lhs) *= rhs; }
+  friend mint operator/(const mint& lhs, const mint& rhs) { return mint(lhs) /= rhs; }
 
-  friend ModInt pow(ModInt mi, int k) { ModInt r = 1; for(; k > 0; k /= 2, mi *= mi) if(k & 1) r *= mi; return r; }
+  friend mint pow(mint mi, int k) { mint r(1); for(; k > 0; k >>= 1, mi *= mi) if(k & 1) r *= mi; return r; }
   static int inv(int mi, int mod) { return mi == 1 ? 1 : mod - inv(mod % mi, mi) * mod / mi; }
-  static int inv(ModInt mi) { 
-    return (int) pow(mi, mod - 2);
+  static mint inv(mint mi) {
+    // return pow(mi, mod - 2);
     // if(gcd(mi.val, mod) != 1) return -1;
-    return inv(mi.val, mod); // use this inv if mod is not prime
+    return mint(inv(mi.val, mod)); // use this inv if mod is not prime
   }
 
-  bool operator==(const ModInt& other) const { return val == other.val; }
-  bool operator!=(const ModInt& other) const { return val != other.val; }
-  friend istream& operator>>(istream& is, ModInt& mi) { is >> mi.val; mi = ModInt(mi.val); return is; }
-  friend ostream& operator<<(ostream& os, const ModInt& mi) { return os << mi.val; }
+  bool operator==(const mint& other) const { return val == other.val; }
+  bool operator!=(const mint& other) const { return val != other.val; }
+  friend istream& operator>>(istream& is, mint& mi) { int x; is >> x; mi = mint(x); return is; }
+  friend ostream& operator<<(ostream& os, const mint& mi) { return os << mi.val; }
 };
-
-using mint = ModInt;
