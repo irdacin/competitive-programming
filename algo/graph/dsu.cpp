65d28b4eea5daa4b9bdce44366a7a3fbe3ac851a
irdacin <nichodri22@gmail.com>
2025-11-26 19:55:19 +0700

    refactor: change void return to bool

--- algo/graph/dsu.cpp
+++ algo/graph/dsu.cpp
@@ -4,16 +4,17 @@ struct DSU {
     iota(all(parent), 0);
   }
 
-  int find(int u){
+  int find(int u) {
     return u == parent[u] ? u : parent[u] = find(parent[u]);
   }
 
-  void unite(int u, int v){
+  bool unite(int u, int v) {
     u = find(u), v = find(v);
-    if(u == v) return;
+    if(u == v) return false;
     
     if(sizes[u] < sizes[v]) swap(u, v);
     sizes[u] += sizes[v];
     parent[v] = u;
+    return true;
   }
 };
